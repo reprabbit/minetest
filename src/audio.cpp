b64f111240759d304ae11679c19ea16227507982
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2011-08-18 07:29:15 +0200

    audio: _CHECK_AVAIL macro

--- src/audio.cpp
+++ src/audio.cpp
@@ -255,10 +255,12 @@ Audio::Audio() :
 }
 
 
+// check if audio is available, returning if not
+#define _CHECK_AVAIL if (!isAvailable()) return
+
 Audio::~Audio()
 {
-	if (!isAvailable())
-		return;
+	_CHECK_AVAIL;
 
 	shutdown();
 }
@@ -299,7 +301,6 @@ static const char *extensions[] = {
 
 std::string Audio::findSoundFile(const std::string &basename, u8 &fmt)
 {
-
 	std::string base(m_path + basename + ".");
 
 	fmt = LOADER_VORBIS;
@@ -318,8 +319,7 @@ std::string Audio::findSoundFile(const std::string &basename, u8 &fmt)
 
 AmbientSound *Audio::getAmbientSound(const std::string &basename)
 {
-	if (!isAvailable())
-		return NULL;
+	_CHECK_AVAIL NULL;
 
 	AmbientSoundMap::Node* cached = m_ambient_sound.find(basename);
 
@@ -345,8 +345,7 @@ AmbientSound *Audio::getAmbientSound(const std::string &basename)
 void Audio::setAmbient(const std::string &slotname,
 		const std::string &basename)
 {
-	if (!isAvailable())
-		return;
+	_CHECK_AVAIL;
 
 	if (m_ambient_slot.find(slotname))
 		((AmbientSound*)(m_ambient_slot[slotname]))->stop();
@@ -376,8 +375,7 @@ void Audio::setAmbient(const std::string &slotname,
 SoundSource *Audio::createSource(const std::string &sourcename,
 		const std::string &basename)
 {
-	if (!isAvailable())
-		return NULL;
+	_CHECK_AVAIL NULL;
 
 	SoundSourceMap::Node* present = m_sound_source.find(sourcename);
 
@@ -405,8 +403,7 @@ SoundSource *Audio::createSource(const std::string &sourcename,
 
 SoundSource *Audio::getSource(const std::string &sourcename)
 {
-	if (!isAvailable())
-		return NULL;
+	_CHECK_AVAIL NULL;
 
 	SoundSourceMap::Node* present = m_sound_source.find(sourcename);
 
@@ -419,6 +416,8 @@ SoundSource *Audio::getSource(const std::string &sourcename)
 
 void Audio::updateListener(const scene::ICameraSceneNode* cam, const v3f &vel)
 {
+	_CHECK_AVAIL;
+
 	v3f pos = cam->getPosition();
 	m_listener[0] = pos.X;
 	m_listener[1] = pos.Y;
@@ -444,8 +443,7 @@ void Audio::updateListener(const scene::ICameraSceneNode* cam, const v3f &vel)
 
 SoundBuffer* Audio::loadSound(const std::string &basename)
 {
-	if (!isAvailable())
-		return NULL;
+	_CHECK_AVAIL NULL;
 
 	u8 fmt;
 	std::string fname(findSoundFile(basename, fmt));
